fb1de7a2250dba346b34f4cf6fe5bf47dc1c3703
KellyEgesa <kelly.egesa@gmail.com>
2021-01-03 00:12:22 +0300

    Finished working on the worksheet

--- src/components/workbook.jsx
+++ src/components/workbook.jsx
@@ -1,7 +1,16 @@
 import React, { Component } from "react";
+var XLSX = require("xlsx");
 
+let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const alphabetSplit = alphabet.split("");
 class WorkBook extends Component {
-  state = { selectedCell: "" };
+  state = {
+    selectedCell: "",
+    col: [],
+    sheet: { 0: [""] },
+  };
+
+  componentDidMount() {}
 
   highlightCell(cell) {
     this.setState({ selectedCell: cell });
@@ -19,10 +28,57 @@ class WorkBook extends Component {
     }
   }
 
+  onKeyPressed(event) {
+    const { sheet } = this.state;
+    let value = document.getElementById(event).innerHTML;
+    const cell = event.split("");
+    let tempArray = [];
+    let row = cell[1];
+    let rowNumber = alphabetSplit.indexOf(row);
+    let column;
+
+    if (cell.length < 4) {
+      column = cell[2];
+    } else {
+      let number = "";
+      for (let i = 2; i < cell.length; i++) {
+        number += cell[i];
+      }
+      column = parseInt(number);
+    }
+
+    if (sheet[column] == null) {
+      let last = parseInt(Object.keys(sheet).pop());
+      for (let index = last + 1; index <= column; index++) {
+        sheet[index] = [];
+      }
+    }
+
+    if (sheet[column][rowNumber] != null) {
+      let tempRow = sheet[column];
+      tempRow.splice(rowNumber, 1, value);
+    } else {
+      if (value === "") {
+        return;
+      }
+      let tempRow = sheet[column];
+      let diff = rowNumber + 1 - tempRow.length;
+      for (let index = 0; index < diff; index++) {
+        tempRow.push("");
+      }
+      tempRow.splice(rowNumber, 1, value);
+    }
+  }
+
+  save() {
+    const { sheet } = this.state;
+    let data = Object.values(sheet);
+    console.log(data);
+  }
+
   render() {
-    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     let colHeader = [];
-    const rowHeader = alphabet.split("");
+    const alphabetSplit = alphabet.split("");
     const { selectedCell } = this.state;
     const selected = selectedCell.split("");
 
@@ -35,7 +91,7 @@ class WorkBook extends Component {
           >
             {index}
           </td>
-          {rowHeader.map((item) => {
+          {alphabetSplit.map((item) => {
             return (
               <td
                 key={item}
@@ -47,7 +103,11 @@ class WorkBook extends Component {
                     : "workArea"
                 }
               >
-                <p contentEditable="true" id={`${item}${index}`}></p>
+                <p
+                  contentEditable="true"
+                  id={`P${item}${index}`}
+                  onBlur={() => this.onKeyPressed(`P${item}${index}`)}
+                ></p>
               </td>
             );
           })}
@@ -57,11 +117,12 @@ class WorkBook extends Component {
 
     return (
       <div>
+        <button onClick={() => this.save()}>Save</button>
         <table>
           <thead>
             <tr>
               <th></th>
-              {rowHeader.map((item) => {
+              {alphabetSplit.map((item) => {
                 return (
                   <th
                     className={
